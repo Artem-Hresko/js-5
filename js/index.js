078c4a51f1a6cffcde0efe74e0050b346fa805ca
Artem Hresko <gresko.artem09@gmail.com>
2025-06-14 20:15:44 +0100

    z

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
 // 1
 
-const drinks = "Чай";
+let selectedDrink = prompt("Оберіть напій (Кава, Чай або Сік):");
 
-switch (drinks) {
-  case "Чай":
-    console.log("Ви обрали чай. Смачного!");
-    break;
+switch (selectedDrink) {
   case "Кава":
-    console.log("Ви обрали каву. Смачного!");
+    alert("Ви обрали каву. Смачного!");
+    break;
+  case "Чай":
+    alert("Ви обрали чай. Насолоджуйтесь!");
     break;
   case "Сік":
-    console.log("Ви обрали cік. Смачного!");
+    alert("Ви обрали сік. Освіжайтесь!");
     break;
   default:
-    console.log("Будь ласка, оберіть напій зі списку.");
+    alert("Будь ласка, оберіть напій зі списку: Кава, Чай або Сік.");
 }
 
 // 2
 
 let days = prompt(
-  "Enter the day of the week, make sure to begin with capital letter"
+  "Enter the day of the week, make sure to begin with capital letter!"
 );
 if (
   days.includes("Monday") ||
@@ -37,4 +37,44 @@ if (
   }
 } else {
   alert("enter the day correctly please");
+}
+
+// 3
+
+let season;
+let monthNumber = Number(prompt("Enter the number of the months"));
+
+switch (monthNumber) {
+  case 1:
+  case 2:
+  case 12:
+    season = "Winter";
+    alert(`The season now is ${season}`);
+
+    break;
+  case 3:
+  case 4:
+  case 5:
+    season = "Spring";
+    alert(`The season now is ${season}`);
+
+    break;
+
+  case 6:
+  case 7:
+  case 8:
+    season = "Summer";
+    alert(`The season now is ${season}`);
+
+    break;
+
+  case 9:
+  case 10:
+  case 11:
+    season = "Autumn";
+    alert(`The season now is ${season}`);
+
+    break;
+  default:
+    alert(" Enter the months number correctly!");
 }
